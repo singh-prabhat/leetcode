23d1a4637ac2771ec638b5d53c5a707fc7c507d2
Prabhat Singh <prabhatsingh@salesforce.com>
2021-02-11 23:10:09 -0800

    Swapping nodes in pair

---
+++ recursion/swapPair.c
@@ -0,0 +1,29 @@
+
+#include <stdio.h>
+
+struct ListNode 
+{
+ int val;
+ struct ListNode *next;
+
+};
+
+
+
+struct ListNode* swapPairs(struct ListNode* head){
+    struct ListNode *tmp1 = NULL;
+    struct ListNode *tmp2 = NULL;
+    struct ListNode *tmp3 = NULL;
+
+    if(tmp1 = head) {
+        if (tmp2 = head->next) {
+                tmp3 = head->next->next;
+            
+                head = tmp2;
+                head->next = tmp1;
+                head->next->next = swapPairs(tmp3);
+        }
+    }
+
+    return head;
+}
